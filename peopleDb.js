6b328a2e67ecdb0907b574dc62f57e90db6d4c97
Clark Williams <theclarkwilliams@gmail.com>
2019-08-09 09:39:49 -0600

    server up and running, people db created

---
+++ peopleDb.js
@@ -0,0 +1,21 @@
+const people = [
+    {
+        id: 1,
+        name: "Faramir",
+        chores: [
+            "Kidnap some small people"
+        ]
+    },
+    {
+        id: 1,
+        name: "Eomer",
+        chores: [
+            "Save Rohan",
+            "Have fabulous eyebrows"
+        ]
+    },
+    {
+        id: 1,
+        name: "Glorfindel"
+    }
+]
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(logger);
+
+server.get('/', (req, res) => {
+    res.status(200).json({
+        Dustin: "it's working!"
+    })
+});
+
+function logger(req, res, next) {
+    const method = req.method;
+    const url = req.url;
+    const timestamp = Date.now();
+    console.log(`${method} request to '${url}' at ${timestamp}`);
+    next()
+};
+
+module.exports = server;
